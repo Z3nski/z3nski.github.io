8f4180000648564bff178b45e87ad5213a854e00
E_Chill <35980944+Eaz3@users.noreply.github.com>
2022-03-09 11:11:48 -0500

    cla

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -137,14 +137,15 @@ var config_data = `
     "endgame": {
       "Climb Level Attempted": {
         "code":"cla",
-        "title": "Climb",
+        "title": "Climb Level Attempted",
         "type":"radio",
         "choices":{
-          "1":"0<br>",
-          "2":"1<br>",
-          "3":"2<br>",
-          "4":"3<br>",
-          "a":"4<br>",
+          "1":"No Climb<br>",
+          "2":"Low<br>",
+          "3":"Mid<br>",
+          "4":"High<br>",
+          "n":"Traversal<br>",
+          "x":"n/a"
         },
         "defaultValue":"x"
       },
