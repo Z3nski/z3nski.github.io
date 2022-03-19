6b168cded80e1112ef89a6a9fd9dc46c5cd44e41
E_Chill <35980944+Eaz3@users.noreply.github.com>
2022-03-18 23:26:20 -0400

    Lowercase miliv

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022MILIV",
+        "defaultValue":"2022miliv",
         "required":"true",
         "disabled":"true"
       },
