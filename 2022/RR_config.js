be0d095c7f6257dc3c8ee9f4b602836f8b1fcf1a
E_Chill <35980944+Eaz3@users.noreply.github.com>
2022-04-13 01:26:21 -0400

    changed event code

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022milak",
+        "defaultValue":"2022micmp4",
         "required":"true",
         "disabled":"true"
       },
