83638dec2bbaa6875a09d12d70fc78b5add58403
E_Chill <35980944+Eaz3@users.noreply.github.com>
2022-04-14 09:51:26 -0400

    event code

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022micmp4",
+        "defaultValue":"micmp4",
         "required":"true",
         "disabled":"true"
       },
