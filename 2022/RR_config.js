037d2d9121f9a09bf5f9fc531df68cff4d1542bb
Z3nski <45806507+Z3nski@users.noreply.github.com>
2022-10-08 08:52:34 -0600

    Update RR_config.js
    2022COKC

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022micmp4",
+        "defaultValue":"2022cokc",
         "required":"true",
         "disabled":"true"
       },
